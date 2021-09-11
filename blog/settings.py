cb0e55b006c9a38bfdc993c7f9c5ab3c0313c85d
Felipe <felipe.ditec@gmail.com>
2021-09-11 13:29:10 -0300

    C4 - deploy and post_busca.html, post_categoria.html, post_detalhes.html

--- blog/settings.py
+++ blog/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 MEDIA_URL = '/media/'
@@ -144,6 +145,16 @@ MESSAGE_TAGS = {
 }
 
 INSTALLED_APPS += ('django_summernote', )
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+try:
+    from .local_settings import *
+except:
+    pass
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
